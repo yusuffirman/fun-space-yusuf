7d4e46c664d8308ce02b9b408dc0cfcb4c05b642
yusuffirman <jatmokosutresno@gmail.com>
2024-09-10 09:41:19 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+# hitung  luas segitiga segitig
+def luas_segitiga():
+     a =int(input("massukan alas segitiga"))
+     t =int(input("massukan tinggi segitiga"))
+     luas = a * t / 2
+     print("luas  segitiga adalah", luas)
+
+luas_segitiga()
